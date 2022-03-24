121055622e6506f6a2182e4a63f03eea4cbaefcc
Hajime Nakagawa <navyblue0914@icloud.com>
2022-03-24 17:02:20 +0900

    feat: title, faviconの変更

--- src/components/page/Top/Page.tsx
+++ src/components/page/Top/Page.tsx
@@ -8,12 +8,15 @@ export const TopPage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Next & daisyUI</title>
+        <title>男気ゴチレース！</title>
         <meta
           name="description"
           content="A template for prototyping with Next.js and daisy UI"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>👊🏻</text></svg>"
+        />
       </Head>
 
       <TopPageView />
