e4597029ab02b99154314baee08d59a41717fd88
Hajime Nakagawa <51704330+HajimexxxNakagawa@users.noreply.github.com>
2022-03-24 17:09:32 +0900

    Revert "feat: title, faviconの変更"

--- src/components/page/Top/Page.tsx
+++ src/components/page/Top/Page.tsx
@@ -8,15 +8,12 @@ export const TopPage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>男気ゴチレース！</title>
+        <title>Next & daisyUI</title>
         <meta
           name="description"
           content="A template for prototyping with Next.js and daisy UI"
         />
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>👊🏻</text></svg>"
-        />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <TopPageView />
