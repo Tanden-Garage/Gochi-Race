04b6673b07cc4c2e4470db6a2e5be19d39aee0a0
Hajime Nakagawa <navyblue0914@icloud.com>
2022-03-24 17:10:44 +0900

    feat: change description

--- src/components/page/Top/Page.tsx
+++ src/components/page/Top/Page.tsx
@@ -9,10 +9,7 @@ export const TopPage: NextPage = () => {
     <>
       <Head>
         <title>男気ゴチレース！</title>
-        <meta
-          name="description"
-          content="A template for prototyping with Next.js and daisy UI"
-        />
+        <meta name="description" content="おい、ここは俺に支払わせろッ！" />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>👊🏻</text></svg>"
